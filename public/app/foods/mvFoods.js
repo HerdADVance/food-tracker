0b5aac83ed7d07f7fe627ffcd2359656f5b367cd
Alex Vance <herdadvance@gmail.com>
2016-01-08 20:38:13 -0600

    Brand Names added

--- public/app/foods/mvFoods.js
+++ public/app/foods/mvFoods.js
@@ -52,7 +52,7 @@ angular.module('foodTracker').factory('mvFoods', function($http, mvIdentity, mvU
 		usdaSearch: function(term){
 			var dfd = $q.defer();
 
-			$http.get('https://api.nutritionix.com/v1_1/search/' + term + '?fields=item_name%2Citem_id%2Cnf_calories%2Cnf_total_fat%2Cnf_protein%2Cnf_total_carbohydrate%2Cnf_sodium%2Cnf_dietary_fiber&appId=5d11d7cb&appKey=1045827ad8f2f2a19d402024fd9319c6').then(function(response){
+			$http.get('https://api.nutritionix.com/v1_1/search/' + term + '?fields=item_name%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_protein%2Cnf_total_carbohydrate%2Cnf_sodium%2Cnf_dietary_fiber&appId=5d11d7cb&appKey=1045827ad8f2f2a19d402024fd9319c6').then(function(response){
 	      if(response.status == 200){
 	      	var usdaSearchResult = response.data.hits;
 	      	dfd.resolve(usdaSearchResult);
@@ -67,7 +67,8 @@ angular.module('foodTracker').factory('mvFoods', function($http, mvIdentity, mvU
 		usdaPackageItem: function(item){
 			console.log(item);
     	var newItem = {};
-    	newItem.name = item.fields.item_name
+    	if(item.fields.brand_name != "USDA") newItem.name = item.fields.brand_name + " " + item.fields.item_name;
+    		else newItem.name = item.fields.item_name;
     	newItem.portion = item.fields.nf_serving_size_qty + " " + item.fields.nf_serving_size_unit;
     	newItem.calories = item.fields.nf_calories;
     	newItem.protein = item.fields.nf_protein;
